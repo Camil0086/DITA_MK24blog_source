d74f486c42898fedfe0b45d17c89a54d8d56696b
Camil0086 <kamil.nowak.0086@gmail.com>
2021-04-23 23:32:16 +0200

    first_update

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+define(function() {var keywords=[{w:"Our",p:["p0"]},{w:"friend",p:["p0"]},{w:"the",p:["p0","p4"]},{w:"tree",p:["p0"]},{w:"Solid",p:["p1","p3"]},{w:"wood",p:["p1","p3"]},{w:"On",p:["p2"]},{w:"trips",p:["p2"]},{w:"to",p:["p2","p4"]},{w:"India",p:["p2"]},{w:"furniture",p:["p3","p4"]},{w:"stands",p:["p3"]},{w:"for",p:["p3"]},{w:"environmental",p:["p3"]},{w:"friendliness",p:["p3"]},{w:"and",p:["p3"]},{w:"sustainability",p:["p3"]},{w:"From",p:["p4"]},{w:"website",p:["p4"]},{w:"living",p:["p4"]},{w:"room,",p:["p4"]},{w:"how",p:["p4"]},{w:"does",p:["p4"]},{w:"my",p:["p4"]},{w:"favorite",p:["p4"]},{w:"piece",p:["p4"]},{w:"of",p:["p4"]},{w:"get",p:["p4"]},{w:"home?",p:["p4"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5];
+ph["p2"]=[6, 7, 8, 9];
+ph["p3"]=[4, 5, 10, 11, 12, 13, 14, 15, 16];
+ph["p4"]=[17, 2, 18, 8, 2, 19, 20, 21, 22, 23, 24, 25, 26, 10, 27, 8, 23, 28];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
